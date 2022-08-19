74cb8cdb1c4648a061b21a85d34b75e15d62622a
Alexander Vasilchenko <sanloid2@gmail.com>
2022-08-20 00:38:08 +0600

    Updates

---
+++ precache-manifest.692fe9e140cc2366edcf59493e30726a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d07ddea6b061519c4ea9",
+    "url": "/CanbanReact/static/js/main.d07ddea6.chunk.js"
+  },
+  {
+    "revision": "6a10769d74e38726d746",
+    "url": "/CanbanReact/static/js/1.6a10769d.chunk.js"
+  },
+  {
+    "revision": "a83d5ac20139c79904c5",
+    "url": "/CanbanReact/static/js/runtime~main.a83d5ac2.js"
+  },
+  {
+    "revision": "16c754c1d60ea583dfe9d5de6e7b2a33",
+    "url": "/CanbanReact/index.html"
+  }
+];
